9e5ece07387872f75abcc2715f7edc1e01a58e6b
João Bittencourt <Joaovitorbittencourt777@gmail.com>
2021-07-23 12:15:12 -0300

    adding MaterialApp to MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,15 @@ main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFFFF6C6F),
+        accentColor: Color(0xFFFFFFFF),
+      ),
+      home: Login(),
+    );
+  }
+}
